5380b315d8e9aa06e9f130ab5775c88055c1a3c1
Raymond Huang <yulirice@gmail.com>
2022-11-25 15:45:55 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
+// Route::get('/', function () {
+//     return view('index');
+// });
+// Route::get('/shop', function(){
+//    return view('shop');
+// });
+
+Route::namespace('App\Http\Controllers')->group(function(){
+  Route::get('/','SiteController@index');
+  Route::get('/shop', 'SiteController@shop');
 });
